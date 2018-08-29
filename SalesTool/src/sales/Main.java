70b5cafd1470225fc9f9e6657cdd679a8df90315
LuxTitanium <LuxTitanium@GrandePapi>
2018-08-29 12:57:02 -0400

    For is active

---
+++ SalesTool/src/sales/Main.java
@@ -0,0 +1,13 @@
+package sales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for( int x = -1; x <= 30; x++) {
+			System.out.println("x = " + x);
+		}
+
+	}
+
+}
